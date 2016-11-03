adff0e4b51d6eba9fd9d2bbc9a478391bb645c34
Corentin Marciau <corentin@marciau.fr>
2016-11-03 23:51:44 +0100

    [DEV] Good comparison operators

--- src/containers/VisibleTuneList.js
+++ src/containers/VisibleTuneList.js
@@ -6,9 +6,9 @@ const getVisibleTunes = (tunes, filter) => {
         case 'SHOW_ALL':
             return tunes
         case 'SHOW_FEUCHATTERTON':
-            return tunes.filter(t => t.artist == "Feu! Chartterton")
+            return tunes.filter(t => t.artist === "Feu! Chartterton")
         case 'SHOW_REST':
-            return tunes.filter(t => t.artist != "Feu! Chartterton")
+            return tunes.filter(t => t.artist !== "Feu! Chartterton")
         default:
             throw new Error('Unknown filter: ' + filter)
     }
